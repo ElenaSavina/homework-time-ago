f2b8d1d852b83d05eb666611cb78f1606995eb15
Elena Savina <savina1289@yandex.ru>
2022-06-24 23:45:52 +0300

    Added function "agoToText"

--- src/main/kotlin/ru/netology/time.kt
+++ src/main/kotlin/ru/netology/time.kt
@@ -1,5 +1,24 @@
 package ru.netology
 
-fun main() {
+const val MINUTE = 60
+const val HOUR = 3_600
+const val DAY = 86_400
+const val TWO_DAYS = 172_800
+const val THREE_DAYS = 259_200
 
+fun main() {
+    val secondsAgoOnline = 145
+    val wasOnline = agoToText(secondsAgoOnline)
+    println(wasOnline)
+}
+
+fun agoToText(secondsAgoOnline: Int): String {
+    return when {
+        secondsAgoOnline <= MINUTE -> "был(а) только что"
+        secondsAgoOnline <= HOUR -> "был(а) в сети х минут назад"
+        secondsAgoOnline <= DAY -> "был(а) в сети x часов назад"
+        secondsAgoOnline <= TWO_DAYS -> "был(а) сегодня"
+        secondsAgoOnline <= THREE_DAYS -> "был(а) вчера"
+        else -> "был(а) давно"
+    }
 }
